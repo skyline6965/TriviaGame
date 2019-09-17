8e4626c7c856a49f70f64bbe2f86ea40a3d5a3d2
Kyle Barnhouse <skyline6965@gmail.com>
2019-09-16 23:25:33 -0500

    final commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,85 @@
-var myQuestions = [
-    {
-        question: "What year did Star Wars: A New Hope come out??",
-        answers: {
-            a: '1966',
-            b: '1977',
-            c: '1981',
-            d: '1976'
-        },
-        correctAnswer: 'b'
-    },
-    {
-        question: "Who owned the millennium falcon before Han Solo?",
-        answers: {
-            a: 'Lando Calrissian',
-            b: 'Chewbaca',
-            c: 'Luke Skywalker',
-            d: 'Gerneral Zarpedon'
-        },
-        correctAnswer: 'a'
-    },
-    {
-        question: "Who did the voice over for Darth Vader?",
-        answers: {
-            a: 'Arnold Schwarzenegger',
-            b: 'Samuel L. Jackson',
-            c: 'James Earl Jones',
-            d: 'Luther Vandross'
-        },
-        correctAnswer: 'c'
-    }
-];
+var number = 120;
+var intervalId;
 
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
+
+
+
+
+var startBtn = document.getElementById("startBtn");
+// var results = document.getElementById("results");
+var score = 0;
+var answers = ["a", "b", "c", "b"];
+var tot = answers.length;
+
+//Start Button to Render Quiz
+function startQuiz() {
+    startBtn.style.display = "none";
+    quiz.style.display = "block";
+}
+
+$("#startBtn").on("click", run)
+
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+
+
+    function decrement() {
+        number--;
+        $("#show-number").html("<p>" + number + "<p>");
+    }
+}
+
+
+// Why is radioName working? Is this a set var for inputs?
+// in the "function checkedValue(radioName)" line, what does calling out radioName in the () do?
+function checkedValue(radioName){
+    var radios = document.getElementsByName(radioName);
+    for(var i=0; i<radios.length; i++)
+      if(radios[i].checked) 
+      return radios[i].value;
+}
+
+
+function getResults(){
+    var score = 0;
+    for (var x=0; x<tot; x++)
+        if(checkedValue("q"+x)===answers[x]) score += 1; 
+        return score;
+  }
+
+  
+  //I hid the var results and in here, it's still calling in the html 'results' id. Why?
+  function returnScore(){
+    quiz.style.display = "none";
+    results.style.display = "block";
+    results.innerHTML = ("Your score is "+ getResults() +"/"+ tot);
+  }
+
+
+//   didn't have time to set stop function and talley score. 
+
+
+
+// function onSubmit() {
+    
+//     var q0 = document.forms["quiz"]["q1"].value;
+//     var q1 = document.forms["quiz"]["q2"].value;
+//     var q2 = document.forms["quiz"]["q3"].value;
+
+//     alert(q0);
+
+//     for(var i = 0)
+
+//     // quiz.style.display = "none";
+    
+// }
+
+
+
+
+
+// click => start game
+// timer starts
+// user answers questions by clicking on button
+// 
